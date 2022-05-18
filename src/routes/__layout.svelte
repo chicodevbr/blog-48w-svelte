6806135b64427d99e0330668675b3a869460d438
chicodevbr <chicodevbr@gmail.com>
2022-05-17 22:41:58 -0300

    feat: :sparkles: config graphql client && add index page && navbar

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import '../app.css';
+  import Nav from '../lib/nav.svelte';
+</script>
+
+<Nav />
+
+<main class="container mx-auto px-4">
+  <slot />
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch('/posts.json');
+    if (res.ok) {
+      const { posts } = await res.json();
+      return {
+        props: {
+          posts,
+        },
+      };
+    }
+  };
+</script>
+
+<script>
+  export let posts;
+</script>
+
+<svelte:head>
+  <title>48 W | Blog</title>
+</svelte:head>
+
+{#each posts as { title, coverImage, slug }}
+  <div class="card lg:card-side bg-base-100 shadow-xl m-24">
+    <figure>
+      <img
+        class="rounded-xl h-96"
+        src={coverImage}
+        alt={`Cover image for ${title}`}
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{title}</h2>
+      <p>Some descriptions here</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-secondary">Read More</button>
+      </div>
+    </div>
+  </div>
+{/each}
